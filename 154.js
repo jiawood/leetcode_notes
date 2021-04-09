fa50aed448d076eb8c3f2f3fb436afd5c0efbd71
xiaojia <17806251528@163.com>
2021-04-09 18:39:23 +0800

    154

---
+++ 154.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+  let n = nums.length
+  let l = 0
+  let r = n - 1 
+  while(l < r){
+      let mid = Math.floor((l + r) / 2)
+      if(nums[mid] > nums[r]){
+          l = mid + 1
+      }else if(nums[mid] === nums[r]){
+          r--
+      }else{
+          r = mid 
+      }
+  }
+  return nums[l]
+};
