feacef3f612b6abeffff779443640280f6b66edb
xiaojia <17806251528@163.com>
2022-01-25 11:15:45 +0800

    1688

---
+++ 1688.js
@@ -0,0 +1,13 @@
+var numberOfMatches = function(n) {
+  if(n === 1){
+      return 0
+  }
+  if(n === 2){
+      return 1
+  }
+  if(n % 2 === 0){
+      return n / 2 + numberOfMatches(n / 2)
+  }else{
+      return Math.floor(n / 2) + numberOfMatches(Math.floor(n / 2) + 1)
+  }
+};
